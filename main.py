6a7ac4f2d86ef3f90aeb05abc084865f8cb0516b
Qing0928 <rs040215@gmail.com>
2023-12-18 18:06:52 +0800

    2023/12/18

--- main.py
+++ main.py
@@ -7,25 +7,31 @@ from mediapipe.framework.formats import landmark_pb2
 import numpy as np
 #----init setting----
 #monitor
-#monitor = cv2.VideoCapture("rtsp://admin:@admin888@192.168.11.197:554")
+monitor = cv2.VideoCapture("rtsp://admin:@admin888@192.168.11.197:554")
+video_width = monitor.get(cv2.CAP_PROP_FRAME_WIDTH)
+video_height = monitor.get(cv2.CAP_PROP_FRAME_HEIGHT)
+print(video_width, video_height)
+fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+video_writer = cv2.VideoWriter('detect_video.mp4', fourcc, 24.0, (1280, 720))
 #import tensorflow as tf
 #print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
-'''while(True):
-    if(monitor.isOpened == False):
-        monitor.open()
-    ret, frame = monitor.read()
-    if(ret == False):
-        print("mointor ERROR")
-        break
-    else:
-        cv2.imshow("mointor", frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+'''# STEP 3: Load the input image.
+image_mat = cv2.imread('monitor/image.jpg')
+#print(image_mat.shape)
+#image_mat = cv2.resize(image_mat, (300, 400), interpolation=cv2.INTER_AREA)
+image = mp.Image(image_format=mp.ImageFormat.SRGB, data=image_mat)
+#image = mp.Image.create_from_file("monitor/test.jpg")
 
-monitor.release()
-cv2.destroyAllWindows()'''
+# STEP 4: Detect pose landmarks from the input image.
+detection_result = detector.detect(image) #檢測結果，一堆座標
+#print(detection_result)
 
-
+#把檢測結果畫上去
+annotated_image = draw_landmarks_on_image(image.numpy_view(), detection_result)
+#cv2.imshow("detect_result", cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
+cv2.imshow("detect_result", annotated_image)
+cv2.waitKey(0)
+'''
 
 def draw_landmarks_on_image(rgb_image, detection_result):
   pose_landmarks_list = detection_result.pose_landmarks
@@ -50,26 +56,40 @@ def draw_landmarks_on_image(rgb_image, detection_result):
       solutions.drawing_styles.get_default_pose_landmarks_style())
   return annotated_image
 
+def async_result(detect_result:mp.tasks.vision.PoseLandmarkerResult, result_image:mp.Image, timestamp:int):
+   detect_result = detect_result
+
 # STEP 2: Create an PoseLandmarker object.
 base_options = python.BaseOptions(model_asset_path='monitor/pose_landmarker_full.task')
 options = vision.PoseLandmarkerOptions(
     base_options=base_options,
-    output_segmentation_masks=True)
+    running_mode=mp.tasks.vision.RunningMode.LIVE_STREAM,
+    result_callback=async_result,
+    output_segmentation_masks=False)
 detector = vision.PoseLandmarker.create_from_options(options)
 
-# STEP 3: Load the input image.
-image_mat = cv2.imread('monitor/image.jpg')
-#print(image_mat.shape)
-#image_mat = cv2.resize(image_mat, (300, 400), interpolation=cv2.INTER_AREA)
-image = mp.Image(image_format=mp.ImageFormat.SRGB, data=image_mat)
-#image = mp.Image.create_from_file("monitor/test.jpg")
+while(True):
+    if(monitor.isOpened == False):
+        monitor.open()
+    ret, frame = monitor.read()
+    if(ret == False):
+        print("mointor ERROR")
+        break
+    else:
+        pt1 = (825, 230) #左上
+        pt2 = (970, 340) #右下
+        #cv2.rectangle(frame, pt1, pt2, (0, 0, 255), 2)
+        #patch = frame[230:340, 825:970, :]
+        #patch = cv2.resize(patch,(200, 120))
+        image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+        
+        detection_result = detector.detect(image)
+        annotated_image = draw_landmarks_on_image(image.numpy_view(), detection_result)
+        video_writer.write(annotated_image)
+        cv2.imshow("mointor", annotated_image)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-# STEP 4: Detect pose landmarks from the input image.
-detection_result = detector.detect(image) #檢測結果，一堆座標
-#print(detection_result)
-
-#把檢測結果畫上去
-annotated_image = draw_landmarks_on_image(image.numpy_view(), detection_result)
-#cv2.imshow("detect_result", cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
-cv2.imshow("detect_result", annotated_image)
-cv2.waitKey(0)
+monitor.release()
+video_writer.release()
+cv2.destroyAllWindows()
